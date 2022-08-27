0080ed695eb9e63dbd9789d677f018de7286c3dc
EmanFun <emanuelfun.95@gmail.com>
2022-08-27 04:49:33 -0300

    27/08 04:49 AM

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    hp:{
+      type: DataTypes.NUMBER, 
+    },
+    attack:{
+      type: DataTypes.NUMBER,
+    },
+    defense:{
+      type: DataTypes.NUMBER,
+    },
+    speed:{
+      type:DataTypes.NUMBER,
+    },
+    height:{
+      type: DataTypes.NUMBER,
+    },
+    weight:{
+      type: DataTypes.NUMBER,
+    }
   });
 };
