5465160ae2ec9aa2f351d558de90105073ee0f66
EmanFun <emanuelfun.95@gmail.com>
2023-02-26 02:23:13 -0300

    more styles

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -13,12 +13,12 @@ export default function Card({id,name,image,types,attack}){
 
     return(
         <div className={style.cardContainer}>
-            <h4>N°{id.length > 3 ? id.slice(0,3) : id}</h4>
+            <h4 className={style.id} >N°{id.length > 3 ? id.slice(0,3) : id}</h4>
             <img className={style.pokeImage} src={image} alt={`Pokemon ${name}`}></img>
             <h3 className={style.name}>{name}</h3>
             <ul >{
                 types.map((e,index)=>{
-                    return typeof e !== 'object' ? (<li className={style.types} key={index} >{e}</li>): (<li key={index}>{e.name}</li>)
+                    return typeof e !== 'object' ? (<li className={style.types}  key={index} >{e}</li>): (<li className={style.types} key={index}>{e.name}</li>)
                 })
             }</ul>
             <p className={style.attackPoints} >Puntos de ataque: {attack} &#9876;</p>
