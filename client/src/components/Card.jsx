809b9c2cdaafdb65bdd1f01217610b809e76fef2
EmanFun <emanuelfun.95@gmail.com>
2023-02-26 01:38:09 -0300

    connected style module in component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import style from '../Style/Card.module.css'
 
 
 
@@ -11,16 +12,16 @@ export default function Card({id,name,image,types,attack}){
 
 
     return(
-        <div>
-            <h4>N°{id}</h4>
-            <img src={image} height='40px' width={'40px'} alt={`Pokemon ${name}`}></img>
-            <h3>{name}</h3>
-            <ul>{
+        <div className={style.cardContainer}>
+            <h4>N°{id.length > 3 ? id.slice(0,3) : id}</h4>
+            <img className={style.pokeImage} src={image} alt={`Pokemon ${name}`}></img>
+            <h3 className={style.name}>{name}</h3>
+            <ul >{
                 types.map((e,index)=>{
-                    return typeof e !== 'object' ? (<li key={index} >{e}</li>): (<li key={index}>{e.name}</li>)
+                    return typeof e !== 'object' ? (<li className={style.types} key={index} >{e}</li>): (<li key={index}>{e.name}</li>)
                 })
             }</ul>
-            <p>Puntos de ataque: {attack}</p>
+            <p className={style.attackPoints} >Puntos de ataque: {attack} &#9876;</p>
         </div>
     )
 
